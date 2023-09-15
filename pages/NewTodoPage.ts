50c2126ac4d22dda8cc252ce2dc4e5776d58aeeb
prabhat1991007 <prabhat19911524@gmail.com>
2023-09-15 12:42:02 +0530

    first commit

---
+++ pages/NewTodoPage.ts
@@ -0,0 +1,29 @@
+import { APIRequestContext, Page } from '@playwright/test';
+import TodoApi from '../apis/TodoApi';
+import User from '../models/User';
+
+export default class NewTodoPage {
+	private get newTodoInput() {
+		return '[data-testid=new-todo]';
+	}
+
+	private get newTodoSubmit() {
+		return '[data-testid=submit-newTask]';
+	}
+
+	async load(page: Page) {
+		await page.goto('/todo/new');
+	}
+
+	async addTodo(page: Page, task: string) {
+		await page.type(this.newTodoInput, task);
+		await page.click(this.newTodoSubmit);
+	}
+
+	async addTodoUsingApi(request: APIRequestContext, user: User) {
+		const res= await new TodoApi().addTodo(request, user);
+		console.log('res',res)
+		const responseBody=await res.json()
+		console.log('resBody',responseBody)
+	}
+}
